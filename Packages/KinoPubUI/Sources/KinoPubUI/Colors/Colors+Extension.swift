a4354f973b0ee23c0b689e8d279f884bf18169e7
Kirill Kunst <kirillkunst@gmail.com>
2023-07-22 00:52:08 +0300

    add uikit colors

---
+++ Packages/KinoPubUI/Sources/KinoPubUI/Colors/Colors+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extension.swift
+//
+//
+//  Created by Kirill Kunst on 22.07.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  struct KinoPub {
+    static let accent = Color("accent_color", bundle: .module)
+    static let accentRed = Color("accent_red_color", bundle: .module)
+    static let background = Color("background_color", bundle: .module)
+    static let text = Color("accent_color", bundle: .module)
+  }
+}
---
+++ Packages/KinoPubUI/Sources/KinoPubUI/Font/Font+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Font+Extension.swift
+//
+//
+//  Created by Kirill Kunst on 22.07.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+  
+}
+
