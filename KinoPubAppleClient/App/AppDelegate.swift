4597f5c2850f4bee0762a239d011200d34e15131
Kirill Kunst <kirillkunst@gmail.com>
2023-08-07 15:36:53 +0300

    show player only in landscape mode

--- KinoPubAppleClient/App/AppDelegate.swift
+++ KinoPubAppleClient/App/AppDelegate.swift
@@ -11,17 +11,23 @@ import FirebaseCore
 
 #if os(iOS)
 class AppDelegate: NSObject, UIApplicationDelegate {
+  static var orientationLock = UIInterfaceOrientationMask.all
+  
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     FirebaseApp.configure()
     return true
+  }
+  
+  func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    return AppDelegate.orientationLock
   }
 }
 #endif
 
 #if os(macOS)
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+  
   func applicationDidFinishLaunching(_ notification: Notification) {
     FirebaseApp.configure()
   }
--- KinoPubAppleClient/Views/Player/PlayerView.swift
+++ KinoPubAppleClient/Views/Player/PlayerView.swift
@@ -14,6 +14,7 @@ struct PlayerView: View {
   @StateObject private var playerManager: PlayerManager
   @State private var hideNavigationBar = false
   @State var activeGestures: GestureMask = .subviews
+  @State private var navBarHidden = false
 
   init(manager: @autoclosure @escaping () -> PlayerManager) {
     _playerManager = StateObject(wrappedValue: manager())
@@ -23,18 +24,27 @@ struct PlayerView: View {
     GeometryReader { _ in
       ZStack {
         VideoPlayer(player: playerManager.player)
-
+          .ignoresSafeArea(.all)
+          .onAppear(perform: {
+            playerManager.player.play()
+          })
       }
     }
     .navigationBarHidden(hideNavigationBar)
     .navigationBarTitle("", displayMode: .inline)
+    .navigationBarHidden(navBarHidden)
     .ignoresSafeArea(.all)
     .toolbar(.hidden, for: .tabBar)
     .onAppear(perform: {
       UIApplication.shared.isIdleTimerDisabled = true
+      UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+      AppDelegate.orientationLock = .landscape
     })
     .onDisappear(perform: {
       UIApplication.shared.isIdleTimerDisabled = false
+      AppDelegate.orientationLock = .all
+      UIDevice.current.setValue(UIDevice.current.orientation.rawValue, forKey: "orientation")
+      UIViewController.attemptRotationToDeviceOrientation()
     })
   }
 }
