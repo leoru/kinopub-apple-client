8bdaa0653906b27fda990d9a4c8d1c4b1b8b1b61
GingerManLTU <49916496+GingerManLTU@users.noreply.github.com>
2025-02-06 10:12:20 +0200

    changeLanguage function added
    Moved changeLanguage function from View to Model.

--- KinoPubAppleClient/Views/Profile/ProfileModel.swift
+++ KinoPubAppleClient/Views/Profile/ProfileModel.swift
@@ -12,32 +12,42 @@ import OSLog
 
 @MainActor
 class ProfileModel: ObservableObject {
-  
-  private var userService: UserService
-  private var errorHandler: ErrorHandler
-  private var authState: AuthState
-  
-  @Published public var userData: UserData = UserData.mock()
-  
-  init(userService: UserService,
-       errorHandler: ErrorHandler,
-       authState: AuthState) {
-    self.userService = userService
-    self.errorHandler = errorHandler
-    self.authState = authState
-  }
-  
-  func fetch() {
-    Task {
-      do {
-        self.userData = try await userService.fetchUserData()
-      } catch {
-        errorHandler.setError(error)
-      }
+    
+    private var userService: UserService
+    private var errorHandler: ErrorHandler
+    private var authState: AuthState
+    
+    @Published public var userData: UserData = UserData.mock()
+    @Published var selectedLanguage: String
+    @Published var shouldShowExitAlert: Bool = false
+    
+    let availableLanguages = ["en": "English", "lt": "Lietuvių", "ru": "Русский"]
+    
+    init(userService: UserService,
+         errorHandler: ErrorHandler,
+         authState: AuthState) {
+        self.userService = userService
+        self.errorHandler = errorHandler
+        self.authState = authState
+        self.selectedLanguage = UserDefaults.standard.string(forKey: "selectedLanguage") ?? Locale.currentLanguageCode
     }
-  }
-  
-  func logout() {
-    authState.logout()
-  }
+    func fetch() {
+        Task {
+            do {
+                self.userData = try await userService.fetchUserData()
+            } catch {
+                errorHandler.setError(error)
+            }
+        }
+    }
+    
+    func logout() {
+        authState.logout()
+    }
+    
+    func changeLanguage(to language: String) {
+        UserDefaults.standard.setValue([language], forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
+        shouldShowExitAlert = true
+    }
 }
